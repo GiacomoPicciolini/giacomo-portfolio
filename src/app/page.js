30d26f43479a0e5c1a0f00cba67dab9889404e09
Matteo Dante <matteo.dante@pilatus-aircraft.com>
2024-09-25 12:15:19 +0200

    arrows

--- src/app/page.js
+++ src/app/page.js
@@ -37,6 +37,7 @@ export default function Home() {
       <Description />
       <Projects />
       <SlidingImages />
+      <Contact />
     </main>
   )
 }
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,48 +4,20 @@ import Rounded from '../../common/RoundedButton';
 import { useRef } from 'react';
 import { useScroll, motion, useTransform, useSpring } from 'framer-motion';
 import Magnetic from '../../common/Magnetic';
+import Link from 'next/link';
 
 export default function index() {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ["start end", "end end"]
-    })
-    const x = useTransform(scrollYProgress, [0, 1], [0, 100])
-    const y = useTransform(scrollYProgress, [0, 1], [-200, 0])
-    const rotate = useTransform(scrollYProgress, [0, 1], [120, 90])
+
     return (
-        <motion.div style={{ y }} ref={container} className={styles.contact}>
+        <motion.div className={styles.contact}>
             <div className={styles.body}>
                 <div className={styles.title}>
                     <h2>Want to</h2>
-                    <h2>Get in touch</h2>
+                    <h2>Get in Touch?</h2>
                 </div>
                 <div className={styles.nav}>
-                    <p>giacomo.picciolini3@gmail.com</p>
-                    <p>+41 76 512 59 32</p>
-                </div>
-                <div className={styles.info}>
-                    <div>
-                        <span>
-                            <h3></h3>
-                            <p>©2024</p>
-                        </span>
-                    </div>
-                    <div>
-                        <span>
-                            <h3>socials</h3>
-                            <Magnetic>
-                                <p>Linkedin</p>
-                            </Magnetic>
-                        </span>
-                        <Magnetic>
-                            <p>Instagram</p>
-                        </Magnetic>
-                        <Magnetic>
-                            <p>Dribbble</p>
-                        </Magnetic>
-                    </div>
+                    <a href='tel:+41 76 512 59 32'><Image width={24} height={24} alt="Open" src={"/arrow_forward.svg"} />+41 76 512 59 32</a>
+                    <a href='mailto:giacomo.picciolini3@gmail.com'><Image width={24} height={24} alt="Open" src={"/arrow_forward.svg"} />giacomo.picciolini3@gmail.com</a>
                 </div>
             </div>
         </motion.div>
