e8e6981b764bf3804f99b4c92f19026ac34b91a5
Matteo Dante <matteo.dante659@gmail.com>
2024-10-01 14:55:06 +0200

    image

--- src/app/page.js
+++ src/app/page.js
@@ -32,6 +32,7 @@ export default function Home() {
       <AnimatePresence mode='wait'>
         {isLoading && <Preloader />}
       </AnimatePresence>
+      <GradientCursor />
       <Landing />
       <Description />
       <Projects />
