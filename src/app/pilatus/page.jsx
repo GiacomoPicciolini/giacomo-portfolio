e8eeee8c245ee034f519f3239b92cc9d55a400bf
Matteo Dante <matteo.dante659@gmail.com>
2024-10-03 14:09:52 +0200

    up

--- src/app/pilatus/page.jsx
+++ src/app/pilatus/page.jsx
@@ -18,10 +18,13 @@ function Section() {
     <div ref={container} className={styles.sectionContainer}>
       <div className={styles.textContainer}>
         <p className={styles.description}>
-          Beauty and quality need the right time to be conceived and realised
-          even in a world that is in too much of a hurry.
+          In collaboration with other Design Teams, we built first the company’ Design System.</p>
+        <p className={styles.heading}>
+          My first Goal was to analyzed how to incorporate Design discipline into an already well-defined development process, with a V-Based approach, and established work-phases that include System Engineers, Project Managers, and V&V.
+          <br /><br />
+          We used the Google Design Sprint methodology, tailoring the process to fit our needs and expectations, as baseline to create our own design process.
+          Introducing workshops, User Research and collaborating with other departments, we were at the end able to fit Design into the Pilatus Lifecycle, starting immediately to see on the products under development results and improvements.
         </p>
-        <p className={styles.heading}>Background Parallax</p>
       </div>
       <div className={styles.background}>
         <motion.div style={{ y }} className={styles.backgroundImage}>
@@ -75,7 +78,7 @@ export default function Page() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.descriptionContainer}>
+      <div className={styles.mainDescriptionContainer}>
         <div className={styles.label}>
           <Image src={"/radio.svg"} width={24} height={24} alt="dot" />
           <p>PROJECT</p>
@@ -85,17 +88,33 @@ export default function Page() {
         </p>
       </div>
       <Intro />
-      <div className={styles.descriptionContainer}>
-        <div className={styles.label}>
-          <Image src={"/radio.svg"} width={24} height={24} alt="dot" />
-          <p>PROJECT</p>
+      <div className={styles.doubleDescriptionContainer}>
+        <div className={styles.secondaryDescription}>
+          <div className={styles.label}>
+            <Image src={"/radio.svg"} width={24} height={24} alt="dot" />
+            <p>OVERVIEW</p>
+          </div>
+          <p className={styles.secondaryDescriptionText}>
+            Just as in any other field, the military aviation sector has felt the need to innovate to keep up with the times, and Pilatus has decided to seize the opportunity.
+            <br /><br />
+            At the beginning of 2022, the &quot;Common Platform&quot; project was launched, aimed at creating new teaching tools based on the latest technologies, improving and integrating established products.
+          </p>
         </div>
-        <p className={styles.descriptionText}>
-          The GBTS department is dedicated to developing software for pilot and technician training, teaching students how to approach aircraft through the use of theoretical lessons, flight simulators, and augmented reality.
-        </p>
+        <div className={styles.secondaryDescription}>
+          <div className={styles.label}>
+            <Image src={"/radio.svg"} width={24} height={24} alt="dot" />
+            <p>CHALLENGES AND GOALS</p>
+          </div>
+          <p className={styles.secondaryDescriptionText}>
+            The ultimate goal is to develop a wide range of products that fully cover every aspect of Training Academy instruction.
+            <br /><br />
+            Theoretical lessons, based on images and 3D aircraft models, VR and XR familiarization devices, mission planning and debriefing tools, full-flight simulators.
+            <br /><br />
+            The real goal was to create this set of products, interconnecting them through a centralized management system. This system, called Academy TMS, not only collects and analyzes data but also schedules the academic year and manages content, training assets and users.          </p>
+        </div>
       </div>
       <Section />
-      <div className={styles.descriptionContainer}>
+      <div className={styles.mainDescriptionContainer}>
         <div className={styles.label}>
           <Image src={"/radio.svg"} width={24} height={24} alt="dot" />
           <p>PROJECT</p>
